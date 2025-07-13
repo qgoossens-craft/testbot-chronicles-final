80f99d7ab523b8d399d6011e123b7da36b8e9bd3
PropaGandalf28 <168652592+PropaGandalf28@users.noreply.github.com>
2025-07-13 19:36:48 +0200

    Add netlify adapter for ssr support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://titusconsulting.be',
   output: 'server', // Server mode for admin functionality
+  adapter: netlify(), // Netlify adapter for SSR
 });
