adc56dbd8a77dee35ef549f6d2abfd66f6e41317
qgoossens-craft <quentin.goossens@craft-it.io>
2025-07-14 14:53:11 +0200

    Added search, tags and reading time functions

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,7 @@ const blog = defineCollection({
     heroImage: z.string().optional(),
     tags: z.array(z.string()).default([]),
     draft: z.boolean().default(false),
+    readingTime: z.number().optional(),
   }),
 });
 
